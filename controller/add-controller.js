24a2ed9c99b7fc19f64595c081b1489589e975aa
anna-sahradyan <a.sahradyan72@gmail.com>
2023-06-11 16:39:44 +0400

    added new file controller and model/Conversation

--- controller/add-controller.js
+++ controller/add-controller.js
@@ -26,3 +26,4 @@ export const getUsers = async (req, res) => {
 
 }
 
+
--- router/add-route.js
+++ router/add-route.js
@@ -1,7 +1,10 @@
 import express from "express";
-import {addUser, getUsers} from "../controller/add-controller.js";
+import {addUser, getUsers } from "../controller/add-controller.js";
+import {newConversation,getConversation} from "../controller/conversation-controller.js";
 const router = express.Router();
 router.route("/add").post( addUser);
+router.route("/conversation/add").post(newConversation);
+router.route("/conversation/get").post(getConversation);
 router.route("/users").get(getUsers);
 
 
